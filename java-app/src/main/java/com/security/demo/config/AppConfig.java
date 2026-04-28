e7621812025354bbbf8fa5ef5175a25a70a09091
sofiakulagina <soooooofiaaaaaa@gmail.com>
2026-04-28 20:51:40 +0300

    cicd

--- java-app/src/main/java/com/security/demo/config/AppConfig.java
+++ java-app/src/main/java/com/security/demo/config/AppConfig.java
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 package com.security.demo.config;
 
 import lombok.Getter;
